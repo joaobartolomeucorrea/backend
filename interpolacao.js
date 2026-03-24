4821ed1443411b84c17476520ff50f6d06494ec9
joaobartolomeucorrea <joao.bartolomeu.correa@escola.pr.gov.br>
2026-03-24 14:40:00 +0000

    versao1

---
+++ interpolacao.js
@@ -0,0 +1,10 @@
+const nome ="Ana";
+const idade ="21";
+const anoatual = 2026;
+
+const anonascimento = anoatual - idade;
+const idadeEmDezAnos = idade + 10;
+
+console.log("O ano de nascimento é: " + anonascimento);
+console.log(`${nome} possui ${idade} anos.`);
+console.log(`Daqui ah 10 anos, terá ${idadeEmDezAnos} anos`);
