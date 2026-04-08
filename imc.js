5ca17da738e64d6fbb2e21288fc1fe38fc983efd
joaobartolomeucorrea <joao.bartolomeu.correa@escola.pr.gov.br>
2026-04-08 14:42:53 +0000

    v1

---
+++ imc.js
@@ -0,0 +1,14 @@
+//calculadora de imc
+
+let nome = "Clara";
+let peso = 65;
+let altura = 1.68;
+
+//função que calcula e retorna imc
+function calcularIMC(peso, altura){
+    let imc = peso / (altura * altura)
+};
+
+//let resultado = calcularIMC(peso, altura)
+
+console.log(`IMC de ${nome}: ${resultado}`);
