b00f7ee19061987ba81ab3605f17844c6149728c
joaobartolomeucorrea <joao.bartolomeu.correa@escola.pr.gov.br>
2026-05-13 13:53:57 +0000

    versao1

---
+++ desafio.js
@@ -0,0 +1,13 @@
+const nomeProduto = "carrinho";
+const precoProduto = 20;
+const lojaProduto = "casaChina";
+const dataCompra = 10.4;
+const vendedorProduto = "juliana";
+
+console.log(`Fui a casa China comprar um ${nomeProduto} e ele custava ${precoProduto}, mas comprei mesmo assim, na nota apareceu ${dataCompra} e ${vendedorProduto}.`);
+
+const descontoProuto = 10;
+const resultado = precoProduto - descontoProuto;
+
+console.log(`O carrinho recebeu um desconto de ${descontoProuto} reais`);
+console.log(`O preço final da compra é de ${resultado}`);
