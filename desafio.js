aecd9e65a44f2d22320ce27c8e73f35855fd3b15
joaobartolomeucorrea <joao.bartolomeu.correa@escola.pr.gov.br>
2026-05-20 13:55:07 +0000

    versao3

--- desafio.js
+++ desafio.js
@@ -6,8 +6,16 @@ const vendedorProduto = "juliana";
 
 console.log(`Fui a casa China comprar um ${nomeProduto} e ele custava ${precoProduto}, mas comprei mesmo assim, na nota apareceu ${dataCompra} e ${vendedorProduto}.`);
 
-const descontoProuto = 10;
-const resultado = precoProduto - descontoProuto;
+const descontoProduto = 20;
+const resultado = precoProduto - descontoProduto;
 
-console.log(`O carrinho recebeu um desconto de ${descontoProuto} reais`);
+console.log(`O carrinho recebeu um desconto de ${descontoProduto} reais`);
 console.log(`O preço final da compra é de ${resultado}`);
+
+const preco = 100;
+const quantidadeProduto = 20;
+const valorCompra = 500;
+console.log(`Um cliente quer levar ${preco}, coneguiu ${quantidadeProduto}`);
+console.log(`Com desconto o valor total a compra ficaria ${valorCompra}`);
+console.log(`Se o produto estiver em promoção o produto estará com ${descontoProduto}`);
+console.log(`Se o produto estiver sem estoque o ${quantidadeProduto} estará em zero.`);
