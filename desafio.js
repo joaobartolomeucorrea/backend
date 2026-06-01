6ba91aabad2c58742e9812fe5d4ece69a2ea190e
joaobartolomeucorrea <joao.bartolomeu.correa@escola.pr.gov.br>
2026-06-01 13:50:16 +0000

    veersao5

--- desafio.js
+++ desafio.js
@@ -18,13 +18,14 @@ const valorCompra = 480;
 console.log(`Um cliente quer levar ${preco}, coneguiu ${quantidadeProduto}`);
 console.log(`Com desconto o valor total a compra ficaria ${valorCompra}`);
 console.log(`Se o produto estiver em promoção o produto estará com ${descontoProduto}`);
-console.log(`Se o produto estiver sem estoque o ${quantidadeProduto} estará em zero.`);
+console.log(`Se o produto estiver sem estoque o ${nomeProduto} estará em zero.`);
 
 const totalCompra = 480;
+const ticket = (nomeProduto + vendedorProduto + totalCompra + dataCompra + "concluida"); 
 
-function totalCompra(verificarCompra) {
+function verificarCompra(totalCompras) {
     const foiFinalizada = compra.status === "concluida";
     return foiFinalizada;
-console.log(`A compra foi finalizada com o total  ${totalCompra} reais.`);
+console.log(`A compra foi concluida com o total  ${totalCompra} reais, e foi entregue o ${ticket} para o cliente.`);
 }
 
